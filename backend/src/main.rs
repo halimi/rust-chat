06fb30764dcd5d3098a4395be7b74a46081156c4
Imre Halasz <halimi@freemail.hu>
2025-05-10 23:25:59 +0200

    Implement users list

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use chrono::Utc;
+use common::WebSocketMessage;
 use rocket::futures::{stream::SplitSink, SinkExt, StreamExt};
 use rocket::tokio::sync::Mutex;
 use rocket::State;
@@ -28,10 +29,36 @@ impl ChatRoom {
             author: format!("User #{}", author_id),
             created_at: Utc::now().naive_utc(),
         };
+        let websocket_message = WebSocketMessage {
+            message_type: common::WebSocketMessageType::NewMessage,
+            message: Some(chat_message),
+            users: None,
+        };
         let mut conns = self.connections.lock().await;
         for (_id, sink) in conns.iter_mut() {
             let _ = sink
-                .send(Message::Text(json!(chat_message).to_string()))
+                .send(Message::Text(json!(websocket_message).to_string()))
+                .await;
+        }
+    }
+
+    pub async fn broadcast_user_list(&self) {
+        let mut conns = self.connections.lock().await;
+        let mut users = vec![];
+
+        for (id, _) in conns.iter() {
+            users.push(format!("User #{}", id));
+        }
+
+        let websocket_message = WebSocketMessage {
+            message_type: common::WebSocketMessageType::UsersList,
+            message: None,
+            users: Some(users),
+        };
+
+        for (_id, sink) in conns.iter_mut() {
+            let _ = sink
+                .send(Message::Text(json!(websocket_message).to_string()))
                 .await;
         }
     }
@@ -49,12 +76,14 @@ fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
             let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
             let (ws_sink, mut ws_stream) = stream.split();
             state.add(user_id, ws_sink).await;
+            state.broadcast_user_list().await;
 
             while let Some(message) = ws_stream.next().await {
                 state.broadcast_message(message?, user_id).await;
             }
 
             state.remove(user_id).await;
+            state.broadcast_user_list().await;
 
             Ok(())
         })
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,63 +1,58 @@
-use common::ChatMessage;
-use web_sys::HtmlTextAreaElement;
+use crate::message_list::MessageList;
+use crate::send_dialog::SendDialog;
+use crate::users_list::UsersList;
+use common::{WebSocketMessage, WebSocketMessageType};
 use yew::prelude::*;
 use yew_hooks::use_websocket;
+
+mod message_list;
+mod send_dialog;
+mod users_list;
 
 #[function_component]
 fn App() -> Html {
     let messages_handle = use_state(Vec::default);
     let messages = (*messages_handle).clone();
-    let new_message_handle = use_state(String::default);
-    let new_message = (*new_message_handle).clone();
+    let users_handle = use_state(Vec::default);
+    let users = (*users_handle).clone();
 
     let ws = use_websocket("ws://127.0.0.1:8000".to_string());
 
     let mut cloned_messages = messages.clone();
     use_effect_with(ws.message.clone(), move |ws_message| {
         if let Some(ws_msg) = &**ws_message {
-            let chat_message: ChatMessage = serde_json::from_str(&ws_msg).unwrap();
-            cloned_messages.push(chat_message);
-            messages_handle.set(cloned_messages);
+            let websocket_message: WebSocketMessage = serde_json::from_str(&ws_msg).unwrap();
+            match websocket_message.message_type {
+                WebSocketMessageType::NewMessage => {
+                    let msg = websocket_message.message.expect("Missing message payload");
+                    cloned_messages.push(msg);
+                    messages_handle.set(cloned_messages);
+                }
+                WebSocketMessageType::UsersList => {
+                    let users = websocket_message.users.expect("Missing users payload");
+                    users_handle.set(users);
+                }
+            }
         }
     });
 
-    let cloned_new_message_handle = new_message_handle.clone();
-    let on_message_change = Callback::from(move |e: Event| {
-        let target = e.target_dyn_into::<HtmlTextAreaElement>();
-        if let Some(textarea) = target {
-            cloned_new_message_handle.set(textarea.value());
-        }
-    });
-
-    let cloned_new_message = new_message.clone();
     let cloned_ws = ws.clone();
-    let on_button_click = Callback::from(move |_: MouseEvent| {
-        cloned_ws.send(cloned_new_message.clone());
-        new_message_handle.set("".to_string());
+    let send_message_callback = Callback::from(move |msg: String| {
+        cloned_ws.send(msg.clone());
     });
 
     html! {
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="list-group">
-                {
-                    messages.iter().map(|m| html! {
-                        <div class="list-group-item list-group-item-action">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5>{m.author.clone()}</h5>
-                                <small>{m.created_at.format("%Y-%m-%d %H:%M:%S").to_string()}</small>
-                            </div>
-                            <p>{m.message.clone()}</p>
-                        </div>
-                    }).collect::<Html>()
-                }
+                <div class="col-sm-3">
+                    <UsersList users={users} />
+                </div>
+                <div class="col-sm-9">
+                    <MessageList messages={messages} />
                 </div>
             </div>
             <div class="row">
-                <div class="input-group">
-                    <textarea class="form-control" onchange={on_message_change} value={new_message}></textarea>
-                    <button type="submit" class="btn-primary" onclick={on_button_click}>{"Send"}</button>
-                </div>
+                <SendDialog send_message_callback={send_message_callback} />
             </div>
         </div>
     }
